659f9d1de969110855b833fde23d5c718f0fe0fe
smalleast <382026180@qq.com>
2018-05-19 16:31:07 +0800

    gy

--- src/vue-copy.js
+++ src/vue-copy.js
@@ -1,67 +1,67 @@
-var Clipboard = require('clipboard/dist/clipboard.min.js') // FIXME: workaround for browserify
+var copy = require('clipboard/dist/clipboard.min.js') // FIXME: workaround for browserify
 
-var VueClipboard = {
+var VueCopy = {
   install: function (Vue) {
     Vue.prototype.$copyText = function (text, container) {
       return new Promise(function (resolve, reject) {
         var fake_el = document.createElement('button');
-        var clipboard = new Clipboard(fake_el, {
+        var copy = new copy(fake_el, {
           text: function () { return text },
           action: function () { return 'copy' },
           container: typeof container === 'object' ? container : document.body
         });
-        clipboard.on('success', function (e) {
-          clipboard.destroy();
+        copy.on('success', function (e) {
+          copy.destroy();
           resolve(e);
         });
-        clipboard.on('error', function (e) {
-          clipboard.destroy();
+        copy.on('error', function (e) {
+          copy.destroy();
           reject(e);
         });
         fake_el.click();
       });
     };
 
-    Vue.directive('clipboard', {
+    Vue.directive('copy', {
       bind: function (el, binding, vnode) {
         if(binding.arg === 'success') {
-          el._v_clipboard_success = binding.value
+          el._v_copy_success = binding.value
         } else if(binding.arg === 'error') {
-          el._v_clipboard_error = binding.value
+          el._v_copy_error = binding.value
         } else {
-          var clipboard = new Clipboard(el, {
+          var copy = new copy(el, {
             text: function () { return binding.value },
             action: function () { return binding.arg === 'cut' ? 'cut' : 'copy' }
           })
-          clipboard.on('success', function (e) {
-            var callback = el._v_clipboard_success
+          copy.on('success', function (e) {
+            var callback = el._v_copy_success
             callback && callback(e)
           })
-          clipboard.on('error', function (e) {
-            var callback = el._v_clipboard_error
+          copy.on('error', function (e) {
+            var callback = el._v_copy_error
             callback && callback(e)
           })
-          el._v_clipboard = clipboard
+          el._v_copy = copy
         }
       },
       update: function (el, binding) {
         if(binding.arg === 'success') {
-          el._v_clipboard_success = binding.value
+          el._v_copy_success = binding.value
         } else if(binding.arg === 'error') {
-          el._v_clipboard_error = binding.value
+          el._v_copy_error = binding.value
         } else {
-          el._v_clipboard.text = function () { return binding.value }
-          el._v_clipboard.action = function () { return binding.arg === 'cut' ? 'cut' : 'copy' }
+          el._v_copy.text = function () { return binding.value }
+          el._v_copy.action = function () { return binding.arg === 'cut' ? 'cut' : 'copy' }
         }
       },
       unbind: function (el, binding) {
         if(binding.arg === 'success') {
-          delete el._v_clipboard_success
+          delete el._v_copy_success
         } else if(binding.arg === 'error') {
-          delete el._v_clipboard_error
+          delete el._v_copy_error
         } else {
-          el._v_clipboard.destroy()
-          delete el._v_clipboard
+          el._v_copy.destroy()
+          delete el._v_copy
         }
       }
     })
@@ -69,9 +69,9 @@ var VueClipboard = {
 }
 
 if (typeof exports == "object") {
-  module.exports = VueClipboard
+  module.exports = VueCopy
 } else if (typeof define == "function" && define.amd) {
   define([], function() {
-    return VueClipboard
+    return VueCopy
   })
 }
